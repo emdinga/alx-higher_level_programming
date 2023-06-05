2c829d86ff019c34e726eeb67a1f7da86a1d48e3
emdinga <emdinga@gmail.com>
2023-06-05 07:41:10 -0700

    0x00. Python - Hello, World

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * check_cycle - Main function to check the list
+ * @list: Parameter value
+ *
+ * REturn: 0 or 1
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *s = list;
+	listint_t *f = list;
+
+	if (!list)
+		return (0);
+	while (s && f && f->next)
+	{
+		s = s->next;
+		f = f->next->next;
+		if (s == f)
+			return (1);
+	}
+	return (0);
+}
