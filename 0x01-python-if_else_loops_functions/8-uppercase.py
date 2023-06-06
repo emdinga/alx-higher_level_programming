a46a00608c623c1fc8431874f638c121a2d3fbd3
emdinga <emdinga@gmail.com>
2023-06-06 12:12:27 -0700

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for a in str:
+        if ord(a) >= ord('a') and ord(a) <= ord('z'):
+            char = chr(ord(c) - 32)
+        else:
+            char = a
+        print("{:s}".format(char), end="")
+    print('')
