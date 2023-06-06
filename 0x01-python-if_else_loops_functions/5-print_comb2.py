7ec0ea30dc956386bfb37796e9d581cfc28ccea1
emdinga <emdinga@gmail.com>
2023-06-06 11:36:05 -0700

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for a in range(100):
-    print(f"{a:02d}", end=", " if a < 99 else "\n")
+for a in range(0, 100):
+    if a == 99:
+        print(a)
+    else:
+        print("{:0>2d}".format(a), end=", ")
