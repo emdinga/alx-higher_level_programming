90535831a78af62ba5bb1e829b2c26e01387f881
emdinga <emdinga@gmail.com>
2023-07-05 08:58:43 -0700

    update

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-Rectangle = __import__('0-rectangle').Rectangle
+class Rectangle:
+    pass
 
-my_rectangle = Rectangle()
-print(type(my_rectangle))
-print(my_rectangle.__dict__)
