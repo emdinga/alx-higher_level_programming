a27ad23dce08589537fe5f97ad1a49e8d2383233
emdinga <emdinga@gmail.com>
2023-07-05 09:16:53 -0700

    update

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -9,6 +9,16 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """ Method that initializes the instance """
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
+
+
+# Create an instance of Rectangle
+my_rectangle = Rectangle()
+
+# Print the type of my_rectangle
+print(type(my_rectangle))  # Output: <class '0-rectangle.Rectangle'>
+
+# Print an empty dictionary
+print({})  # Output: {}
 
