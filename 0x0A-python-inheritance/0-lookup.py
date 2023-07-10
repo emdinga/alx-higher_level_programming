5288d5d8f9a319acf8e8504084571473406b95f4
emdinga <emdinga@gmail.com>
2023-07-10 09:40:57 -0700

    update

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+
+"""
+Finding a list of available attributes & methods of an object
+"""
+
 def lookup(obj):
     """
     Returns a list of available attributes and methods of an object.
