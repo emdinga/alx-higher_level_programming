de4b25db5e01b5af5e6e339c28d2c71e599298c3
Emdinga <emdinga@gmail.com>
2023-07-18 12:08:10 -0700

    update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,153 +1,46 @@
 #!/usr/bin/python3
-"""
-Square class
-"""
+"""Defines the Square class."""
 
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """
-    Square class, inherits from Rectangle.
-    """
+    """Represent a square."""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """
-        Initializes an instance of the Square class.
-
-        Args:
-            size (int): Size of the square.
-            x (int): x-coordinate of the square's position.
-            y (int): y-coordinate of the square's position.
-            id (int): ID value for the instance.
-        """
+        """Initialize a square."""
         super().__init__(size, size, x, y, id)
 
-        def update(self, *args, **kwargs):
-        """
-        Assigns attributes to the square.
+    def __str__(self):
+        """Return a string representation of the square."""
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
 
-        Args:
-            *args: No-keyworded arguments for the id, size, x, and y attributes.
-            **kwargs: Keyworded arguments for the attributes of the square.
-        """
+    @property
+    def size(self):
+        """Get or set the size of the square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """Update the square with new attributes."""
         if args:
             attrs = ["id", "size", "x", "y"]
             for i, arg in enumerate(args):
                 setattr(self, attrs[i], arg)
         else:
-            if kwargs:
-                for key, value in kwargs.items():
-                    setattr(self, key, value)
-
-    def __str__(self):
-        """
-        Returns a string representation of the Square instance.
-
-        Returns:
-            str: String representation of the instance.
-        """
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
-
-    @property
-    def width(self):
-        """
-        Getter method for the width attribute.
-        """
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """
-        Setter method for the width attribute.
-
-        Args:
-            value (int): New width value.
-        """
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        elif value <= 0:
-            raise ValueError("width must be > 0")
-        else:
-            self.__width = value
-
-    @property
-    def height(self):
-        """
-        Getter method for the height attribute.
-        """
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """
-        Setter method for the height attribute.
-
-        Args:
-            value (int): New height value.
-        """
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        elif value <= 0:
-            raise ValueError("height must be > 0")
-        else:
-            self.__height = value
-
-    @property
-    def x(self):
-        """
-        Getter method for the x attribute.
-        """
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        """
-        Setter method for the x attribute.
-
-        Args:
-            value (int): New x value.
-        """
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        elif value < 0:
-            raise ValueError("x must be >= 0")
-        else:
-            self.__x = value
-
-    @property
-    def y(self):
-        """
-        Getter method for the y attribute.
-        """
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        """
-        Setter method for the y attribute.
-
-        Args:
-            value (int): New y value.
-        """
-        if not isinstance(value, int):
-            raise TypeError("y must be an integer")
-        elif value < 0:
-            raise ValueError("y must be >= 0")
-        else:
-            self.__y = value
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
     def to_dictionary(self):
-        """
-        Returns the dictionary representation of the Rectangle instance.
-
-        Returns:
-            dict: Dictionary representation of the instance.
-        """
+        """Return the dictionary representation of the square."""
         return {
             "id": self.id,
-            "width": self.width,
-            "height": self.height,
+            "size": self.size,
             "x": self.x,
             "y": self.y
         }
