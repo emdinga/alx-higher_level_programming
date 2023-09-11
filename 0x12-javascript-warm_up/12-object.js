c8a38b02b286825e761c84df3a5600a780919aff
emdinga <emdinga@gmail.com>
2023-09-11 12:25:19 -0700

    javascript

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+let myValue = 12;
+myValue = 89;
+console.log(`The updated value is ${myValue}`);
---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+global.add = function add(a, b) {
+  return a + b;
+};
