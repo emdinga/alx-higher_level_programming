6443bdbc9deda75d73772e65c18e5ceeaa4e14de
emdinga <emdinga@gmail.com>
2023-09-17 01:27:50 -0700

    relational mappping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,10 +3,9 @@
 This script lists all states from the database hbtn_0e_0_usa.
 """
 
-import sys
-import MySQLdb
-
 if __name__ == "__main__":
+    import sys
+    import MySQLdb
 
     if len(sys.argv) != 4:
         print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
@@ -25,7 +24,7 @@ if __name__ == "__main__":
     
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states ORDER BY states.id")
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC,")
 
     results = cursor.fetchall()
     
