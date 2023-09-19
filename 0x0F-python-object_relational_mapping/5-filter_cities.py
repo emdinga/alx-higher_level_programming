f92d0a70f2023919bc9563112108159f77596875
emdinga <emdinga@gmail.com>
2023-09-19 05:22:15 -0700

    relational

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -2,18 +2,15 @@
 """
 This script lists all cities of a specified state from the hbtn_0e_4_usa database.
 """
-
 import sys
 import MySQLdb
 
+
 if __name__ == "__main__":
-
     if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
+        print('Usage: {} <username> <password> <database> <state_name>'.format(sys.argv[0]))
         sys.exit(1)
-
     username, password, database, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
-
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -21,19 +18,14 @@ if __name__ == "__main__":
         passwd=password,
         db=database
     )
-
     cursor = db.cursor()
-
-    query = "SELECT cities.id, cities.name, states.name FROM cities INNER JOIN\
-    states ON cities.state_id = states.id WHERE states.name =\
-    %s ORDER BY cities.id"
-    cursor.execute(query, (state_name,))
-
+    cursor.execute("SELECT GROUP_CONCAT(name SEPARATOR ', ') "
+                   "FROM cities "
+                   "JOIN states ON cities.state_id = states.id "
+                   "WHERE states.name = %s "
+                   "ORDER BY cities.id ASC", (state_name,))
     results = cursor.fetchall()
-
     for row in results:
         print(row)
-
     cursor.close()
     db.close()
-
