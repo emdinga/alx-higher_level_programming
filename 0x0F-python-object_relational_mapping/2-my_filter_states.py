06cec8b186f229f8eb2096b0eee37101879d73f8
emdinga <emdinga@gmail.com>
2023-09-19 02:58:41 -0700

    relational

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,19 +5,9 @@ This script displays all values in the states table where the name matches the p
 
 import sys
 import MySQLdb
-
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print('Usage: {} <username> <password> <database> <state_name>'.format(sys.argv[0]))
-        sys.exit(1)
-    username, password, database, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+            passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cursor = db.cursor()
     query = "SELECT * FROM states WHERE name = '{}' ORDER BY id".format(state_name)
     cursor.execute(query)
