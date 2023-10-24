cf283f5ae7953981e05138c92325cad8989e00b8
root <Emdinga>
2023-10-24 03:57:41 -0700

    webscraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,21 +1,10 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-if (process.argv.length !== 4) {
-  console.error('Usage: ./1-writeme.js <file-path> <text-to-write>');
-  process.exit(1);
-}
-
-const filePath = process.argv[2];
-const textToWrite = process.argv[3];
-
-fs.writeFile(filePath, textToWrite, 'utf-8', (error) => {
-  if (error) {
-    console.error(error);
-    process.exit(1);
+const file = process.argv[2];
+const content = process.argv[3];
+fs.writeFile(file, content, 'utf-8', function (err) {
+  if (err) {
+    console.log(err);
   }
-
-  console.log(`The text has been written to ${filePath}`);
 });
 
